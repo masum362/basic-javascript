867c289713eb8962bdfe68334ad0c54b4af80c88
masum362 <masumahmed64077@gmail.com>
2024-08-14 15:05:59 +0600

    discus about all basic js

---
+++ script.js
@@ -0,0 +1,22 @@
+// variable learning start here
+
+// let a, b , c; // block scope variable
+let a, b, c; // decleared variable
+// const c = 30; // block scope variable
+
+b = 30; //assign variable
+c = 20;
+
+// console.log(b);
+const d = b + c;
+console.log("hello world");
+console.log(d);
+
+// variable learning end here
+
+a = 10; // statement
+a = 10; // assign
+a = 20; // assign
+
+
+// let 1hedjfds = 1;
